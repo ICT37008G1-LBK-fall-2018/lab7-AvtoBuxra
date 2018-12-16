7ac72a0deb1ff4a3ea81b26e465ddd3de8d54d27
avto <buxrashviliavto@gmail.com>
2018-12-16 21:23:45 +0400

    lab 7 avto

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 const $ = require("jQuery");
 
 $(".btn").click(function(){
  	$("body").css("background-color", "red");
+=======
+const $ = require("jQuery");
+
+$(".btn").click(function(){
+ 	$("body").css("background-color", "red");
+>>>>>>> c2538615422125705fb41239e93006d9bd42ab7a
 })
